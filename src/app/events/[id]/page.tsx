17fdb99ed2b117955fb6c2e6228bd09a63bebd9f
Anderson Otavio <otavios@ctr.com>
2024-01-05 15:14:43 -0300

    consulta api

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,3 +1,6 @@
+import * as api from "@/api/site";
+import { redirect } from "next/navigation";
+
 type Props = {
   params: {
     id: string;
@@ -5,13 +8,17 @@ type Props = {
 }
 
 
-const Page = ({params}: Props) => {
+const Page = async ({params}: Props) => {
+
+  const eventItem = await api.getEvent(parseInt(params.id));
+  if(!eventItem || !eventItem.status) return redirect('/');
+  
   return (
     <main className="text-center mx-auto max-w-lg p-5">
       <header>
         <h2 className="text-2xl text-yellow-400">Secret Friend</h2>
-        <h1 className="text-3xl mt-5 mb-2"> ** EVENT TITLE **</h1>
-        <p className="text-sm mb-5" > ** EVENT DESCRIPTION **</p>
+        <h1 className="text-3xl mt-5 mb-2">{eventItem.title}</h1>
+        <p className="text-sm mb-5" >{eventItem.description}</p>
       </header>
 
       ...
